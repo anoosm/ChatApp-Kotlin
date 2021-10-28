dbd875c04f3d145e671b0bd25a8cdcb67af219cb
Anoos M <anoosalathur@gmail.com>
2021-10-28 07:20:48 +0530

    wireframed the sign up button to sign up page

--- app/src/main/java/com/example/chatappkt/LogIn.kt
+++ app/src/main/java/com/example/chatappkt/LogIn.kt
@@ -1,11 +1,32 @@
 package com.example.chatappkt
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class LogIn : AppCompatActivity() {
+
+    private lateinit var edtEmail: EditText
+    private lateinit var edtPassword: EditText
+    private lateinit var btnLogIn: Button
+    private lateinit var btnSignUp: Button
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_log_in)
+
+        edtEmail = findViewById(R.id.edt_email)
+        edtPassword = findViewById(R.id.edt_password)
+        btnLogIn = findViewById(R.id.btn_login)
+        btnSignUp = findViewById(R.id.btn_signup)
+
+        btnSignUp.setOnClickListener {
+
+            val intent = Intent(this, SignUp::class.java)
+            startActivity(intent)
+        }
     }
 }
